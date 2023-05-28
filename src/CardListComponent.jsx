a04fbbb493a0752ef93da42fa6b1df427409a744
AyushVe99 <Ayushnishu407@gmail.com>
2023-05-28 20:03:49 +0530

    New

---
+++ src/CardListComponent.jsx
@@ -0,0 +1,111 @@
+import React, { useState, useEffect } from "react";
+import "./CardList.css";
+import Navbar from "./Navbar";
+
+const CardListComponent = () => {
+  const [data, setData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
+  const [searchText, setSearchText] = useState("");
+  const [isFiltered, setIsFiltered] = useState(false);
+  const [viewType, setViewType] = useState("table");
+  const [sortKey, setSortKey] = useState("");
+
+  useEffect(() => {
+    fetchData().then((data) => {
+      setData(data);
+      setFilteredData(data);
+    });
+  }, []);
+
+  const fetchData = async () => {
+    const response = await fetch("https://coralmango.com/api/react-test");
+    const data = await response.json();
+    return data;
+  };
+
+  const handleSearch = (e) => {
+    const value = e.target.value;
+    setSearchText(value);
+    filterData(value);
+  };
+
+  const filterData = (value) => {
+    if (value) {
+      const filtered = data.filter((item) =>
+        item.name.toLowerCase().includes(value.toLowerCase())
+      );
+      setFilteredData(filtered);
+      setIsFiltered(true);
+    } else {
+      setFilteredData(data);
+      setIsFiltered(false);
+    }
+  };
+  
+  const handleSort = (key) => {
+    let sortedData = [...filteredData];
+    sortedData.sort((a, b) => {
+      if (a[key] < b[key]) return -1;
+      if (a[key] > b[key]) return 1;
+      return 0;
+    });
+    setFilteredData(sortedData);
+    setSortKey(key);
+  };
+  const handleToggle = () => {
+    setViewType(viewType === "table" ? "card" : "table");
+  };
+
+  return (
+    <div>
+        <Navbar/>
+      <input
+        type="text"
+        placeholder="Search by name..."
+        value={searchText}
+        onChange={handleSearch}
+        className="searchInput"
+      />
+      {/* {isFiltered && <p>You are viewing filtered results.</p>} */}
+      <button onClick={handleToggle}>
+        Switch to {viewType === "table" ? "Card" : "Table"} View
+      </button>
+      <div className={viewType === "table" ? "table-view" : "card-list"}>
+        {viewType === "table" ? (
+          <table>
+            <thead>
+              <tr>
+                <th onClick={() => handleSort("name")}>
+                  Name 🔽{sortKey === "name" }
+                </th>
+                <th onClick={() => handleSort("age")}>
+                  Age 🔽{sortKey === "age" }
+                </th>
+                <th>Occupation</th>
+              </tr>
+            </thead>
+            <tbody>
+              {filteredData.map((item, index) => (
+                <tr key={index}>
+                  <td>{item.name}</td>
+                  <td>{item.age}</td>
+                  <td>{item.occupation}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        ) : (
+          filteredData.map((item, index) => (
+            <div key={index} className="card">
+              <h3>{item.name}</h3>
+              <p>Age: {item.age}</p>
+              <p>Occupation: {item.occupation}</p>
+            </div>
+          ))
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default CardListComponent;
---
+++ src/LoginPage.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import "./App.css"
+const LoginPage = ({ handleLogin }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (username === "demo@coralmango.com" && password === "demo123") {
+      handleLogin();
+    } else {
+      setError("Invalid Credentials!");
+    }
+  };
+
+  return (
+    <div className="loginContainer">
+      <div className="loginDiv">
+      <h2>Login Page</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Username : </label>
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Password : </label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit" className="loginBtn">Login</button>
+        {error && <p>{error}</p>}
+      </form>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
